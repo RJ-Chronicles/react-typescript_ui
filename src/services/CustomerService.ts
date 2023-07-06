42d312c3f08c8adf05647cf81ca28b1dc6a4124e
Jondhalekar <2106957@cognizant.com>
2023-07-06 07:53:41 +0530

    added billing model api

--- src/services/CustomerService.ts
+++ src/services/CustomerService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-const CUSTOMER_BASE_URL = "http://localhost:3001/customer/";
-//const CUSTOMER_BASE_URL =
-//  "https://kallyankar-api-service.onrender.com/customer/";
+//const CUSTOMER_BASE_URL = "http://localhost:3001/customer/";
+const CUSTOMER_BASE_URL =
+  "https://kallyankar-api-service.onrender.com/customer/";
 class CustomerService {
   getListOfCustomer(headers: any) {
     console.log("inside");
