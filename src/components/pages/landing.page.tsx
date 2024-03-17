c8961683a366d5e120da2f16219c632b46ca71b5
Jondhalekar <2106957@cognizant.com>
2024-03-17 12:07:48 +0530

    Header component and some more features

--- src/components/pages/landing.page.tsx
+++ src/components/pages/landing.page.tsx
@@ -2,92 +2,17 @@ import img1 from "../../img/team/img1.jpg";
 import landing_page_battery from "../../img/battery_landing_page.jpg";
 import Footer from "../UI/landingpage/footer";
 import Contact from "../UI/landingpage/contact";
-import { Link } from "react-router-dom";
 
 import { ReactComponent as Cloud } from "../svg/cloud.svg";
 import { ReactComponent as Car } from "../svg/car-alt.svg";
 import { ReactComponent as Gear } from "../svg/cog.svg";
 import { ReactComponent as Gift } from "../svg/gift.svg";
-import logo from "../../img/logo.png";
+
+import Header from "./LandingPage/index";
 const Landingpage = () => {
   return (
     <div>
-      <section id="hero">
-        <div className=" mb-12">
-          <div
-            id="carouselExampleCaptionsFull"
-            className="carousel slide carousel-fade relative h-screen"
-            data-bs-ride="carousel"
-          >
-            <div className=" px-6  absolute z-10 right-0 left-0 md:px-16 mx-auto">
-              <div className=" md:mt-5">
-                <nav className="py-2 flex justify-between items-center relative duration-300">
-                  <a className="navbar-brand" href="index.html">
-                    <img src={logo} alt="Logo" />
-                  </a>
-
-                  <div className="header-btn  sm:block sm:absolute sm:right-0 sm:mr-16 lg:static lg:mr-0">
-                    <Link
-                      className="text-blue-600 font-bold border-2 border-blue-400 px-10 py-3 rounded-full duration-300 hover:bg-blue-300 hover:text-white"
-                      to="/admin-login"
-                    >
-                      Login
-                    </Link>
-                  </div>
-                </nav>
-              </div>
-            </div>
-
-            <div className="carousel-inner relative w-full overflow-hidden h-screen">
-              <div className="carousel-item active relative float-left w-full h-screen bg-no-repeat bg-cover bg-center">
-                <video
-                  className="min-w-full min-h-full max-w-fit xl:min-w-0 xl:min-h-0"
-                  playsInline
-                  autoPlay
-                  muted
-                  loop
-                >
-                  <source
-                    className=""
-                    src="https://mdbootstrap.com/img/video/Lines.mp4"
-                    type="video/mp4"
-                  />
-                  {/* <!-- https://mdbootstrap.com/img/video/Lines.mp4"  images/videos/landingpage.mp4 --> */}
-                </video>
-                <div
-                  className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                  style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
-                >
-                  <div className="flex justify-center items-center h-full">
-                    <div className="text-slate-200 text-center py-8 px-8 md:py-16 md:px-14">
-                      <h2 className="animate-pulse font-bold tracking-wide font-mono text-5xl">
-                        Kalyankar Battery
-                      </h2>
-                      <h5 className="text-xl md:text-2xl font-normal mt-12 md:max-w-3xl text-gray-400 mb-6">
-                        Our thousands of customers assure us that we provide the
-                        best ever batteries all over the district.
-                      </h5>
-                      <div className="md:space-x-2 group group:hover:bg-blue-800">
-                        <a
-                          type="button"
-                          className="inline-block px-6 py-2 mb-2 md:mb-0 border-2 border-green-600 text-zinc-100 font-medium text-xs leading-tight uppercase rounded animate-bounce hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out hover:bg-blue-900 hover:text-white"
-                          href="#contact"
-                          role="button"
-                          data-mdb-ripple="true"
-                          data-mdb-ripple-color="light"
-                        >
-                          Contact Us
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
+      <Header />
       <div id="feature" className="bg-white py-24">
         <div className="container">
           <div className="flex flex-wrap items-center">
